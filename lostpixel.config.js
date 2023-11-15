60302c4e2a68aaf8a1f313a92dc87252445aca70
kaito <dung.hoang@codeenginestudio.com>
2023-11-15 15:32:23 +0700

    test: testing regression button story book with lost-pixel

---
+++ lostpixel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  storybookShots: {
+    storybookUrl: "./storybook-static",
+  },
+
+  lostPixelProjectId: "cloz9c7wb6vkhek0ew2ipucie",
+  apiKey: "958af338-0ef4-4765-9748-74a6563ed08c",
+};
